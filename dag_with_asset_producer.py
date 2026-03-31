25b69c00b05bb64aaab1721186bcd2c314809621
AndreyEremin02 <99276280+AndreyEremin02@users.noreply.github.com>
2026-03-31 11:45:24 +0300

    Create dag_with_asset_producer.py

---
+++ dag_with_asset_producer.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+from airflow.datasets import Dataset
+
+numbers_dataset = Dataset("postgres://numbers_table")
+
+def update_numbers():
+    hook = PostgresHook(postgres_conn_id='postgres-production')
+
+    hook.run("""
+        TRUNCATE TABLE numbers;
+
+        INSERT INTO numbers (value) VALUES
+        (5), (15), (25), (35), (45);
+    """)
+
+with DAG(
+    dag_id="producer_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    update_task = PythonOperator(
+        task_id="update_numbers",
+        python_callable=update_numbers,
+        outlets=[numbers_dataset]  # 👈 публикуем asset
+    )
