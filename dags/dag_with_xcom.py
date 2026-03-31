d1945ed0ae277fafd7aa179ab01a3ca339caa161
AndreyEremin02 <99276280+AndreyEremin02@users.noreply.github.com>
2026-03-31 12:41:13 +0300

    Create dag_with_xcom.py

---
+++ dags/dag_with_xcom.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def extract():
+    data = {"numbers": [1, 2, 3, 4, 5]}
+    print(f"Extracted: {data}")
+    return data
+
+def transform(**context):
+    ti = context["ti"]
+
+    data = ti.xcom_pull(task_ids="extract_task")
+
+    numbers = data["numbers"]
+    result = sum(numbers)
+
+    print(f"Sum: {result}")
+    return result
+
+def load(**context):
+    ti = context["ti"]
+
+    result = ti.xcom_pull(task_ids="transform_task")
+
+    print(f"Final result: {result}")
+
+with DAG(
+    dag_id="xcom_demo_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False
+) as dag:
+
+    extract_task = PythonOperator(
+        task_id="extract_task",
+        python_callable=extract
+    )
+
+    transform_task = PythonOperator(
+        task_id="transform_task",
+        python_callable=transform
+    )
+
+    load_task = PythonOperator(
+        task_id="load_task",
+        python_callable=load
+    )
+
+    # зависимости
+    extract_task >> transform_task >> load_task
