985cb2798a5304e6dcba87774cc4eb0994deeefb
AndreyEremin02 <99276280+AndreyEremin02@users.noreply.github.com>
2026-04-05 14:22:05 +0300

    Create dag_with_sensor.py

---
+++ dags/dag_with_sensor.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import os
+
+def process_file():
+    file_path = "/opt/airflow/data/trigger.txt"
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            content = f.read()
+        print(f"{content}")
+    else:
+        print("Error")
+
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+
+with DAG(
+    dag_id="file_sensor_full_demo",
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["demo", "sensor"],
+) as dag:
+
+    wait_for_file = FileSensor(
+        task_id="wait_for_file",
+        filepath="/opt/airflow/data/trigger.txt",
+        poke_interval=10,
+        timeout=300,
+        mode="reschedule",
+    )
+
+    process = PythonOperator(
+        task_id="process_file",
+        python_callable=process_file,
+    )
+
+    wait_for_file >> process
